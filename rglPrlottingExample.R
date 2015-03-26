90468934b5de832db5d0067a117749e99be2dfac
John Zarifis <johnzarifis@gmail.com>
2015-03-26 12:38:52 +0200

    more testing

--- rglPrlottingExample.R
+++ rglPrlottingExample.R
@@ -24,6 +24,7 @@ my_surface(f, alpha=.2 )
 plot3d(data.fcr$Temp,data.fcr$FCR,data.fcr$AvWeight,type="p", col="red", xlab="Temp", ylab="FCR", zlab="Av. Weigth" )
 
 
+
 # second approach
 nhanes_body <- read.csv("~/Project_FCR/nhanes_body.txt")
 View(nhanes_body)
@@ -58,7 +59,7 @@ fcr.fit <- loess(data.fcr.tr$FCR ~ data.fcr.tr$Temp * data.fcr.tr$AvWeight, span
                     family = 'gaussian')
 
 Temp.vals <- seq(from = 11, to = 27, by = 1)
-AvWeight.vals <- seq(from = 1, to = 1000, by = 20)
+#AvWeight.vals <- seq(from = 1, to = 1000, by = 20)
 AvWeight.vals <-c(0.75,2.5,6,11,16,26.5,47.5,70,90,125,175,225,275,325,425)
 predicted.FCR <- predict(fcr.fit, newdata = expand.grid(AvWeight = AvWeight.vals,Temp = Temp.vals ))
 predFCR = data.frame(predicted.FCR)
@@ -69,6 +70,20 @@ persp( AvWeight.vals,Temp.vals, predicted.FCR, theta = 40, xlab =
 plot3d(AvWeight.vals,Temp.vals,predicted.FCR,type="p", col="red", xlab="Temp", ylab="FCR", zlab="Av. Weigth" )
 surface3d(AvWeight.vals,Temp.vals,predicted.FCR)
 
+surface3d(AvWeight.vals,Temp.vals,predicted.FCR)
+
+plot3d(AvWeight.vals,Temp.vals,predicted.FCR,type="p", col="red", xlab="Temp", ylab="FCR", zlab="Av. Weigth" )
+plot3d(data.fcr$Temp,data.fcr$AvWeight,data.fcr$FCR,type="p", col="green", xlab="Temp", ylab="FCR", zlab="Av. Weigth" )
+
+
+
+Temp.vals <- unique(data.fcr$Temp)
+AvWeight.vals <-unique(data.fcr$AvWeight)
+#AvWeight.vals <- seq(from = 1, to = 1000, by = 20)
+#AvWeight.vals <-c(0.75,2.5,6,11,16,26.5,47.5,70,90,125,175,225,275,325,425)
+predicted.FCR <- predict(fcr.fit, newdata = expand.grid(AvWeight = AvWeight.vals,Temp = Temp.vals ))
+predFCR = data.frame(predicted.FCR)
+View(predFCR)
 
 
 
